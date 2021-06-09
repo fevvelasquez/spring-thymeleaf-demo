8a77c720b3fd4747acb6727172b1fbec7e0c9eb2
fevvelasquez <fevvelasquez@gmail.com>
2021-06-09 16:21:17 -0500

    0.0.2. Model, @ModelAtrribute
    Sending data from model to the view.

--- src/main/java/me/fevvelasquez/spring/mvc/th/demo/controllers/IndexController.java
+++ src/main/java/me/fevvelasquez/spring/mvc/th/demo/controllers/IndexController.java
@@ -1,7 +1,12 @@
 package me.fevvelasquez.spring.mvc.th.demo.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
  * Spring MVC Thymeleaf Demo
@@ -14,7 +19,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
 	@GetMapping({ "/", "/index" })
-	public String index() {
+	public String index(Model model) {
+		// Model only for this handler.
+		model.addAttribute("count", Arrays.asList(101, 102, 103));
+
 		return "index"; // "index.html" located in "src/main/resources/templates"
+	}
+
+	/**
+	 *  This model is common to all handlers.
+	 */
+	@ModelAttribute("letters")
+	private List<String> setLetterList() {
+		return Arrays.asList("A", "B", "C");
 	}
 }
